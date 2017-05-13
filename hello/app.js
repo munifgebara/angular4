33ba7448cdb825580838b84d5b774179a74d2d91
munifgebara <munifgebara@gmail.com>
2017-05-12 23:39:19 -0300

    Hello

---
+++ hello/app.js
@@ -0,0 +1,31 @@
+(function() {
+
+   var AppComponent = ng.core.Component({
+       selector: 'my-app',
+       template: '<h1>Hello World!</h1>'
+
+
+
+
+   })
+   .Class({
+       constructor: function() { }
+
+   });
+
+   var AppModule = ng.core.NgModule({
+       imports: [ng.platformBrowser.BrowserModule],
+       declarations:[AppComponent],
+       bootstrap: [AppComponent]
+   })
+   .Class({
+       constructor:function() { }
+   });
+
+   ng.platformBrowserDynamic.platformBrowserDynamic()
+    .bootstrapModule(AppModule);
+
+
+
+
+})();
